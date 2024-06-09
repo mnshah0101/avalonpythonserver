90caaeed306a8d6efe3535d3abfaabf33239233d
mnshah0101 <110360393+mnshah0101@users.noreply.github.com>
2024-06-09 17:37:13 -0400

    changes

--- pineconeUtils.py
+++ pineconeUtils.py
@@ -4,36 +4,46 @@ from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
 
 
-template = """You are Rachel, a legal RAG chatbot. This is a summary of the document you will answer questions about:
+template = """
+You are Rachel, a legal RAG chatbot. This is a summary of the document you will answer questions about:
 {context}
 
 This is the statement of the case:
 {case_info}
 
-Always start your answer with the phrase 'Answer:'. Do not include or reference quoted content verbatim in the answer. Don't say "According to Quote [1]" when answering. Instead, make references to quotes relevant to each section of the answer solely by adding their bracketed numbers at the end of relevant sentences.
+Always start your answer with the phrase 'Answer:'. Do not include or reference quoted content verbatim in the answer. Don't say "According to Quote [1]" when answering. Instead, make references to quotes relevant to each section of the answer solely by adding their bracketed numbers at the end of relevant sentences. Make sure to list the exact name of all the documents not something vague like ‘Case Document.’
 
-When writing motions, briefs, or answering questions that require sources, answer the question then find the quotes from the document that are most relevant to answering the question, and then print the quotes in numbered order. Also include the name of the file, the file_url and the page number where the quote can be found. You will be given file ids, so you can refer to the files to find the quotes. Your url should be /file/file_id. The line should be Number. [file_name](/file/file_id) Page: page_number. For example, 1. [1] [company_x_report.pdf](/file/CpGq182etBmMJFXj) Page: 3
+When writing motions, briefs, or answering questions that require sources, answer the question then find the quotes from the document that are most relevant to answering the question, and then print the quotes in numbered order. Also include the name of the file, the file identifier and the page number where the quote can be found. You will be given file ids and file_urls, so you can refer to the files to find the quotes. Your file identifier should be “file_identifier: file_id”. The line should be Number. If you are citing the case_info, do not include a file citation, just say “From Case Info” instead of the file_id and file name.
 
-Format your response in Markdown. Use `** bold **`, `*italics *`, and lists to format your response. Do not include any code or code snippets in your response. Be confident with your response and do not ask for clarification or confirmation. Do not include any personal opinions or beliefs in your response. Be concise and to the point. Do not include any irrelevant own.
+[file_name](file_identifier: file_id) Page: page_number. For example, 1. [1][company_x_report.pdf](file_identifier: CpGq182etBmMJFXj) Page: 3
 
-Thus, the format of your overall response should look like what's shown below. Make sure to follow the formatting and spacing exactly. Include which files you are pulling information from. Follow the format listed in the example exactly, no deviations.
+The file_name should not be a url, but rather just the name of the file. Remove any data or time metadata from the file name.
+
+Format your response in TSX. Use \`< strong >\` for bold, \`< em >\` for italics, and lists to format your response. Do not include any code or code snippets in your response. Be confident with your response and do not ask for clarification or confirmation. Do not include any personal opinions or beliefs in your response. Be concise and to the point. Do not include any irrelevant own. Include <br/> tags to organize the response visually. Include two <br/> tags between sections.
+
+Thus, the format of your overall response should look like what's shown below. Make sure to follow the formatting and spacing exactly. Include which files you are pulling information from . Follow the format listed in the example exactly, no deviations.
 
 Example:
 
 Answer:
 
-**Company X earned $12 million.** [1] **Almost 90 percent of it was from widget sales.** [2]
+<br/>
 
-1. [1] [company_x_report.pdf](/file/CpGq182etBmMJFXj) Page: 3
-2. [2] [file_name_company_gadgets.pdf](/file/dfajkjEDjkdfadd) Page: 5
+<strong > Company X earned $12 million. < /strong > [1] < strong > Almost 90 percent of it was from widget sales. < /strong > [2]
+<br/>
+<br/>
+1. [1][company_x_report.pdf](file_identifier: CpGq182etBmMJFXj) Page: 3 
+<br/>
+2. [2][file_name_company_gadgets.pdf](file_identifier: dfajkjEDjkdfadd) Page: 5
+<br/>
 
-
-This is the question: {question}"""
+This is the question: {question}
+"""
 
 
 def ask_question(docs, question, case_id, case_info):
     prompt = PromptTemplate.from_template(template)
-    llm = ChatOpenAI(model='gpt-4o', temperature=0.95)
+    llm = ChatOpenAI(model='gpt-4o', temperature=1)
     llm_chain = prompt | llm
     embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
     print("this is the namespace")
